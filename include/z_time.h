95e9f2d0be56831df36f4532ec6314ecc4f1d096
whiteapolo <darkpendraw@proton.me>
2026-08-14 12:22:32 +0300

    yay

--- include/z_time.h
+++ include/z_time.h
@@ -6,6 +6,7 @@
 typedef clock_t Z_Clock;
 
 clock_t z_get_clock(void);
+double z_clock_get_elapsed_seconds(Z_Clock start);
 void z_print_elapsed_seconds(clock_t start);
 
 #endif
--- src/z_time.c
+++ src/z_time.c
@@ -6,8 +6,13 @@ Z_Clock z_get_clock(void)
     return clock();
 }
 
+double z_clock_get_elapsed_seconds(Z_Clock start)
+{
+    return ((double)(z_get_clock() - start)) / CLOCKS_PER_SEC;
+}
+
 void z_print_elapsed_seconds(Z_Clock start)
 {
-    double elapsed_seconds = ((double)(z_get_clock() - start)) / CLOCKS_PER_SEC;
+    double elapsed_seconds = z_clock_get_elapsed_seconds(start);
     printf("%lf\n", elapsed_seconds);
 }
