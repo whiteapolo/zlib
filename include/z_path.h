9d2f6b8ac4d0baebc08c14e248f8e910ed04c151
whiteapolo <darkpendraw@proton.me>
2026-09-15 20:18:59 +0300

    actions are always ambigious

--- include/z_path.h
+++ include/z_path.h
@@ -6,5 +6,7 @@
 
 bool z_expand_tilde(Z_String_View pathname, Z_String *out);
 bool z_compress_tilde(Z_String_View pathname, Z_String *out);
+bool z_is_directory(const char *pathname);
+bool z_is_regular_file(const char *pathname);
 
 #endif
--- src/z_path.c
+++ src/z_path.c
@@ -1,5 +1,6 @@
 #include <z_path.h>
 #include <z_env.h>
+#include <sys/stat.h>
 
 bool z_expand_tilde(Z_String_View pathname, Z_String *out)
 {
@@ -21,8 +22,30 @@ bool z_compress_tilde(Z_String_View pathname, Z_String *out)
         z_str_append_str(out, pathname);
         return false;
     }
-    
+
     z_str_append_cstr(out, "~");
     z_str_append_str(out, z_sv_advance(pathname, strlen(home)));
     return true;
+}
+
+bool z_is_directory(const char *pathname)
+{
+    struct stat sb;
+
+    if (stat(pathname, &sb) == 0) {
+        return S_ISDIR(sb.st_mode);
+    }
+
+    return false;
+}
+
+bool z_is_regular_file(const char *pathname)
+{
+    struct stat sb;
+
+    if (stat(pathname, &sb) == 0) {
+        return S_ISREG(sb.st_mode);
+    }
+
+    return false;
 }
